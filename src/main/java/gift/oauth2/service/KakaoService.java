3aac49233644616d9fa5f04839ec45ad4c4336d0
jinwon1234 <ljw0626@naver.com>
2025-07-24 15:59:20 +0900

    fix: 타임아웃 환경 설정 분리

--- src/main/java/gift/oauth2/service/KakaoService.java
+++ src/main/java/gift/oauth2/service/KakaoService.java
@@ -32,10 +32,6 @@ public class KakaoService {
         this.jwtUtil = jwtUtil;
         this.objectMapper = objectMapper;
 
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(5_000);
-        requestFactory.setReadTimeout(5_000);
-
         this.restClient = builder
                 .defaultStatusHandler(httpStatusCode -> {
                     if (httpStatusCode.getStatusCode().is4xxClientError() || httpStatusCode.getStatusCode().is5xxServerError()) {
@@ -44,7 +40,6 @@ public class KakaoService {
                     }
                     return true;
                 })
-                .requestFactory(requestFactory)
                 .build();
     }
 
